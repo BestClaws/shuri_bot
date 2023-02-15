0a77e1de9ba022de531524ce1fb005c071799c7a
Best <40067018+BobyClaws@users.noreply.github.com>
2023-02-16 01:26:26 +0530

    use naive time instead of dattime

--- src/last_seen.rs
+++ src/last_seen.rs
@@ -1,19 +1,12 @@
-use chrono::{Utc, TimeZone};
+use chrono::{NaiveDateTime, Utc};
+use serenity::model::{channel::Message, event::TypingStartEvent};
 use serenity::prelude::*;
-use serenity::{model::{channel::Message, event::TypingStartEvent}};
 use sqlx::Row;
 
 use crate::Bot;
 
 pub async fn record_typing_event(bot: &Bot, event: TypingStartEvent) {
-    // typing_event.timestamp is in seconds not millis so convert into millis first.
-    let t = Utc
-        .timestamp_millis_opt(event.timestamp as i64 * 1000)
-        .unwrap();
-
-    println!("{}", event.timestamp);
-
-    // let t = NaiveDateTime::from_timestamp_millis(typing_event.timestamp as i64).unwrap(); // TODO: u64 time to i64 hm..anything u can do?
+    let t = NaiveDateTime::from_timestamp_opt(event.timestamp as i64, 0);
 
     let res = sqlx::query("insert into last_seen (user_id, last_seen) values($1, $2) on conflict (user_id) do update  set last_seen = $2")
         .bind(event.user_id.to_string())
@@ -25,11 +18,10 @@ pub async fn record_typing_event(bot: &Bot, event: TypingStartEvent) {
     println!("{res:?}");
 }
 
-
-pub async fn process_message(bot: &Bot, ctx: Context, msg: Message) {
+pub async fn process_message(bot: &Bot, ctx: &Context, msg: &Message) {
     if msg.content.starts_with("!seen ") {
-        for member in msg.mentions {
-  
+        for member in &msg.mentions {
+            // TODO: learn what exactly for in does or calls. and how references and owned values behave.
 
             let x = sqlx::query("SELECT last_seen FROM last_seen WHERE user_id = $1")
                 .bind(member.id.to_string())
@@ -46,9 +38,4 @@ pub async fn process_message(bot: &Bot, ctx: Context, msg: Message) {
             }
         }
     }
-}
-
-
-pub async fn fetch_last_seen() {
-
 }
